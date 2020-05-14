d8dba979efdcfa3757ef3a470c52381054df57ca
chandra687 <65339580+chandra687@users.noreply.github.com>
2020-05-14 15:13:45 +0530

    Add files via upload

---
+++ cnn.py
@@ -0,0 +1,71 @@
+# Image Classification
+
+# Import libraries
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import GlobalAveragePooling2D
+from keras.layers import Dense
+
+# Initalize CNN
+classifier = Sequential()
+
+# Add 2 convolution layers
+classifier.add(Conv2D(filters=32, kernel_size=(3,3), input_shape=(64, 64, 3), activation='relu'))
+classifier.add(Conv2D(filters=32, kernel_size=(3,3), activation='relu'))
+
+# Add pooling layer
+classifier.add(MaxPooling2D(pool_size=(2,2)))
+
+# Add 2 more convolution layers
+classifier.add(Conv2D(filters=32, kernel_size=(3,3), activation='relu'))
+classifier.add(Conv2D(filters=32, kernel_size=(3,3), activation='relu'))
+
+# Add max pooling layer
+classifier.add(MaxPooling2D(pool_size=(2,2)))
+
+# Add 2 more convolution layers
+classifier.add(Conv2D(filters=32, kernel_size=(3,3), activation='relu'))
+classifier.add(Conv2D(filters=32, kernel_size=(3,3), activation='relu'))
+
+# Add max pooling layer
+classifier.add(MaxPooling2D(pool_size=(2,2)))
+
+# Add global average pooling layer
+classifier.add(GlobalAveragePooling2D())
+
+# Add full connection
+classifier.add(Dense(units=2, activation='softmax'))
+
+# Compiling the ANN
+classifier.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Fit CNN to images
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_set = train_datagen.flow_from_directory(
+        r'F:\CNN_class\Cat-Dog-CNN-Classifier-master\Cat-Dog-CNN-Classifier-master\dataset\training_set',
+        target_size=(64, 64),
+        batch_size=32,
+        class_mode='categorical')
+
+test_set = test_datagen.flow_from_directory(
+        r'F:\CNN_class\Cat-Dog-CNN-Classifier-master\Cat-Dog-CNN-Classifier-master\dataset\test_set',
+        target_size=(64, 64),
+        batch_size=32,
+        class_mode='categorical')
+
+classifier.fit_generator(
+        train_set,
+        steps_per_epoch=8000,
+        epochs=25,
+        validation_data=test_set,
+        validation_steps=2000)
